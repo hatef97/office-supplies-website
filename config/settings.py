874387d39291bf4a498b51f7ef9beb794f226bad
Hatef Barin <amir.barin76@gmail.com>
2025-02-25 16:49:07 +0330

    Fix ALLOWED_HOSTS issue in settings

--- config/settings.py
+++ config/settings.py
@@ -26,9 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
-
+DEBUG = os.getenv('DEBUG', 'True')
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
@@ -97,7 +96,7 @@ DATABASES = {
         'NAME': os.getenv('NAME'),
         'USER': os.getenv('USER'),
         'PASSWORD': os.getenv('PASSWORD'),
-        'HOST': os.getenv('HOST', '127.0.0.1'),  # Change to 127.0.0.1
+        'HOST': os.getenv('HOST', '127.0.0.1'), 
         'PORT': os.getenv('PORT', '3306'),
     }
 }
