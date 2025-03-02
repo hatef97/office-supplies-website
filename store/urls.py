b3174e939df5b9e8fe5982b7577018f0516f33c3
Hatef Barin <amir.barin76@gmail.com>
2025-03-02 23:25:54 +0330

    Add carts & orders to urls

--- store/urls.py
+++ store/urls.py
@@ -10,18 +10,22 @@ router.register(r'categories', views.CategoryViewSet, basename='category')
 router.register(r'products', views.ProductViewSet, basename='product')
 router.register(r'cutomers', views.CustomerViewSet, basename='customer')
 router.register(r'pages', views.PageContentViewSet, basename='pagecontent')
+router.register(r'carts', views.CartViewSet, basename='cart')
+router.register(r'orders', views.OrderViewSet, basename='order')
 router.register(r'team', views.TeamMemberViewSet, basename='teammember')
 
 
 product_router = routers.NestedDefaultRouter(router, r'categories', lookup='category')
 product_router.register(r'products', views.ProductViewSet, basename='category-products')
 
+cart_items_router = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
+cart_items_router.register('items', views.CartItemViewSet, basename='cart-items')
 
 products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
 products_router.register('comments', views.CommentViewSet, basename='product-comments')
 
 
-urlpatterns = router.urls + products_router.urls + [
+urlpatterns = router.urls + products_router.urls + cart_items_router.urls + [
     path('about/', views.AboutView.as_view(), name='about'),
     path('terms/', views.TermsView.as_view(), name='terms'),
 ]
