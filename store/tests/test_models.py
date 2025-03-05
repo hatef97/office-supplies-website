76579e9926725e6046b0ebc24d71a6ac217a1010
Hatef Barin <amir.barin76@gmail.com>
2025-03-05 18:43:12 +0330

    Add tests for CategoryModel

--- store/tests/test_models.py
+++ store/tests/test_models.py
@@ -1,1 +1,35 @@
-# add tests for models
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from store.models import *
+from decimal import Decimal
+
+User = get_user_model()
+
+
+
+class CategoryModelTest(TestCase):
+
+    def setUp(self):
+        self.category = Category.objects.create(
+            name="Electronics",
+            description="All kinds of electronic items."
+        )
+
+    def test_category_creation(self):
+        """Test if a Category instance is created correctly."""
+        self.assertEqual(self.category.name, "Electronics")
+        self.assertEqual(self.category.description, "All kinds of electronic items.")
+
+    def test_string_representation(self):
+        """Test the __str__ method of Category."""
+        self.assertEqual(str(self.category), "Electronics")
+
+    def test_name_uniqueness(self):
+        """Test that the name field must be unique."""
+        with self.assertRaises(Exception):  
+            Category.objects.create(name="Electronics", description="Duplicate name test")
+
+    def test_description_blank(self):
+        """Test that description can be left blank."""
+        category = Category.objects.create(name="Books")
+        self.assertEqual(category.description, "")  # Should default to blank
